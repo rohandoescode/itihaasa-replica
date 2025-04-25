747bf4590b332494f24fa51b4267dac001272da7
rohandoescode <rohanjo290505@gmail.com>
2025-04-25 18:39:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './App.css';
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to itihaasa</h1>
+        <Button variant="contained" color="primary">
+          Explore Now
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
